daccab52491d3a15991fe2c67fe5f276ee3fde72
Roger Schaer <roger.schaer@hevs.ch>
2020-09-01 16:09:41 +0200

    Add comics

--- App.js
+++ App.js
@@ -4,11 +4,17 @@ import { LoadingContext } from "./shared/LoadingContext";
 import storage from "./utils/storage";
 import RootDrawer from "./routes/Drawer";
 import { NavigationContainer } from "@react-navigation/native";
+import { Asset } from "expo-asset";
 //import { Auth2Provider } from "./shared/LoginMidataContext";
 
 export default function App() {
   const [language, setLanguage] = useState(1); //French default
   const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    prefetchImages();
+  }, []);
+
   useEffect(() => {
     async function fetchLanguage() {
       let languageLocal = await storage.getLanguageSetting();
@@ -30,4 +36,67 @@ export default function App() {
     </LoadingContext.Provider>
     //</Auth2Provider>
   );
+}
+
+async function prefetchImages() {
+  await Asset.loadAsync([
+    require("./assets/resources/img/fr/1_fr.jpg"),
+    require("./assets/resources/img/fr/2_fr.jpg"),
+    require("./assets/resources/img/fr/3_fr.jpg"),
+    require("./assets/resources/img/fr/4_fr.jpg"),
+    require("./assets/resources/img/fr/5_fr.jpg"),
+    require("./assets/resources/img/fr/6_fr.jpg"),
+    require("./assets/resources/img/fr/7_fr.jpg"),
+    require("./assets/resources/img/fr/8_fr.jpg"),
+    require("./assets/resources/img/fr/9_fr.jpg"),
+    require("./assets/resources/img/fr/10_fr.jpg"),
+    require("./assets/resources/img/fr/11_fr.jpg"),
+    require("./assets/resources/img/fr/12_fr.jpg"),
+    require("./assets/resources/img/fr/13_fr.jpg"),
+    require("./assets/resources/img/fr/14_fr.jpg"),
+    require("./assets/resources/img/fr/15_fr.jpg"),
+    require("./assets/resources/img/fr/16_fr.jpg"),
+    require("./assets/resources/img/fr/17_fr.jpg"),
+    require("./assets/resources/img/fr/18_fr.jpg"),
+    require("./assets/resources/img/fr/19_fr.jpg"),
+    require("./assets/resources/img/fr/20_fr.jpg"),
+    require("./assets/resources/img/fr/21_fr.jpg"),
+    require("./assets/resources/img/fr/22_fr.jpg"),
+    require("./assets/resources/img/fr/23_fr.jpg"),
+    require("./assets/resources/img/fr/24_fr.jpg"),
+    require("./assets/resources/img/fr/25_fr.jpg"),
+    require("./assets/resources/img/fr/26_fr.jpg"),
+    require("./assets/resources/img/fr/27_fr.jpg"),
+    require("./assets/resources/img/fr/28_fr.jpg"),
+    require("./assets/resources/img/fr/29_fr.jpg"),
+    require("./assets/resources/img/it/1_it.jpg"),
+    require("./assets/resources/img/it/2_it.jpg"),
+    require("./assets/resources/img/it/3_it.jpg"),
+    require("./assets/resources/img/it/4_it.jpg"),
+    require("./assets/resources/img/it/5_it.jpg"),
+    require("./assets/resources/img/it/6_it.jpg"),
+    require("./assets/resources/img/it/7_it.jpg"),
+    require("./assets/resources/img/it/8_it.jpg"),
+    require("./assets/resources/img/it/9_it.jpg"),
+    require("./assets/resources/img/it/10_it.jpg"),
+    require("./assets/resources/img/it/11_it.jpg"),
+    require("./assets/resources/img/it/12_it.jpg"),
+    require("./assets/resources/img/it/13_it.jpg"),
+    require("./assets/resources/img/it/14_it.jpg"),
+    require("./assets/resources/img/it/15_it.jpg"),
+    require("./assets/resources/img/it/16_it.jpg"),
+    require("./assets/resources/img/it/17_it.jpg"),
+    require("./assets/resources/img/it/18_it.jpg"),
+    require("./assets/resources/img/it/19_it.jpg"),
+    require("./assets/resources/img/it/20_it.jpg"),
+    require("./assets/resources/img/it/21_it.jpg"),
+    require("./assets/resources/img/it/22_it.jpg"),
+    require("./assets/resources/img/it/23_it.jpg"),
+    require("./assets/resources/img/it/24_it.jpg"),
+    require("./assets/resources/img/it/25_it.jpg"),
+    require("./assets/resources/img/it/26_it.jpg"),
+    require("./assets/resources/img/it/27_it.jpg"),
+    require("./assets/resources/img/it/28_it.jpg"),
+    require("./assets/resources/img/it/29_it.jpg"),
+  ]);
 }
--- screens/SubScreen.js
+++ screens/SubScreen.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useMemo } from "react";
 import {
   StyleSheet,
   View,
@@ -14,10 +14,200 @@ import { LanguageContext } from "../shared/LanguageContext";
 import utilities from "../utils/utilities";
 import storage from "../utils/storage";
 import ButtonView from "../components/ButtonView";
+import { Asset } from "expo-asset";
+
+function getAssetsMap(assets) {
+  return assets.reduce((acc, asset) => {
+    acc[`${asset.name}.${asset.type}`] = asset.localUri;
+    return acc;
+  }, {});
+}
+
+function getComicsAssets() {
+  let imageAssets = [];
+  imageAssets.push(
+    Asset.fromModule(require("../assets/resources/img/fr/1_fr.jpg"))
+  );
+  imageAssets.push(
+    Asset.fromModule(require("../assets/resources/img/fr/2_fr.jpg"))
+  );
+  imageAssets.push(
+    Asset.fromModule(require("../assets/resources/img/fr/3_fr.jpg"))
+  );
+  imageAssets.push(
+    Asset.fromModule(require("../assets/resources/img/fr/4_fr.jpg"))
+  );
+  imageAssets.push(
+    Asset.fromModule(require("../assets/resources/img/fr/5_fr.jpg"))
+  );
+  imageAssets.push(
+    Asset.fromModule(require("../assets/resources/img/fr/6_fr.jpg"))
+  );
+  imageAssets.push(
+    Asset.fromModule(require("../assets/resources/img/fr/7_fr.jpg"))
+  );
+  imageAssets.push(
+    Asset.fromModule(require("../assets/resources/img/fr/8_fr.jpg"))
+  );
+  imageAssets.push(
+    Asset.fromModule(require("../assets/resources/img/fr/9_fr.jpg"))
+  );
+  imageAssets.push(
+    Asset.fromModule(require("../assets/resources/img/fr/10_fr.jpg"))
+  );
+  imageAssets.push(
+    Asset.fromModule(require("../assets/resources/img/fr/11_fr.jpg"))
+  );
+  imageAssets.push(
+    Asset.fromModule(require("../assets/resources/img/fr/12_fr.jpg"))
+  );
+  imageAssets.push(
+    Asset.fromModule(require("../assets/resources/img/fr/13_fr.jpg"))
+  );
+  imageAssets.push(
+    Asset.fromModule(require("../assets/resources/img/fr/14_fr.jpg"))
+  );
+  imageAssets.push(
+    Asset.fromModule(require("../assets/resources/img/fr/15_fr.jpg"))
+  );
+  imageAssets.push(
+    Asset.fromModule(require("../assets/resources/img/fr/16_fr.jpg"))
+  );
+  imageAssets.push(
+    Asset.fromModule(require("../assets/resources/img/fr/17_fr.jpg"))
+  );
+  imageAssets.push(
+    Asset.fromModule(require("../assets/resources/img/fr/18_fr.jpg"))
+  );
+  imageAssets.push(
+    Asset.fromModule(require("../assets/resources/img/fr/19_fr.jpg"))
+  );
+  imageAssets.push(
+    Asset.fromModule(require("../assets/resources/img/fr/20_fr.jpg"))
+  );
+  imageAssets.push(
+    Asset.fromModule(require("../assets/resources/img/fr/21_fr.jpg"))
+  );
+  imageAssets.push(
+    Asset.fromModule(require("../assets/resources/img/fr/22_fr.jpg"))
+  );
+  imageAssets.push(
+    Asset.fromModule(require("../assets/resources/img/fr/23_fr.jpg"))
+  );
+  imageAssets.push(
+    Asset.fromModule(require("../assets/resources/img/fr/24_fr.jpg"))
+  );
+  imageAssets.push(
+    Asset.fromModule(require("../assets/resources/img/fr/25_fr.jpg"))
+  );
+  imageAssets.push(
+    Asset.fromModule(require("../assets/resources/img/fr/26_fr.jpg"))
+  );
+  imageAssets.push(
+    Asset.fromModule(require("../assets/resources/img/fr/27_fr.jpg"))
+  );
+  imageAssets.push(
+    Asset.fromModule(require("../assets/resources/img/fr/28_fr.jpg"))
+  );
+  imageAssets.push(
+    Asset.fromModule(require("../assets/resources/img/fr/29_fr.jpg"))
+  );
+  imageAssets.push(
+    Asset.fromModule(require("../assets/resources/img/it/1_it.jpg"))
+  );
+  imageAssets.push(
+    Asset.fromModule(require("../assets/resources/img/it/2_it.jpg"))
+  );
+  imageAssets.push(
+    Asset.fromModule(require("../assets/resources/img/it/3_it.jpg"))
+  );
+  imageAssets.push(
+    Asset.fromModule(require("../assets/resources/img/it/4_it.jpg"))
+  );
+  imageAssets.push(
+    Asset.fromModule(require("../assets/resources/img/it/5_it.jpg"))
+  );
+  imageAssets.push(
+    Asset.fromModule(require("../assets/resources/img/it/6_it.jpg"))
+  );
+  imageAssets.push(
+    Asset.fromModule(require("../assets/resources/img/it/7_it.jpg"))
+  );
+  imageAssets.push(
+    Asset.fromModule(require("../assets/resources/img/it/8_it.jpg"))
+  );
+  imageAssets.push(
+    Asset.fromModule(require("../assets/resources/img/it/9_it.jpg"))
+  );
+  imageAssets.push(
+    Asset.fromModule(require("../assets/resources/img/it/10_it.jpg"))
+  );
+  imageAssets.push(
+    Asset.fromModule(require("../assets/resources/img/it/11_it.jpg"))
+  );
+  imageAssets.push(
+    Asset.fromModule(require("../assets/resources/img/it/12_it.jpg"))
+  );
+  imageAssets.push(
+    Asset.fromModule(require("../assets/resources/img/it/13_it.jpg"))
+  );
+  imageAssets.push(
+    Asset.fromModule(require("../assets/resources/img/it/14_it.jpg"))
+  );
+  imageAssets.push(
+    Asset.fromModule(require("../assets/resources/img/it/15_it.jpg"))
+  );
+  imageAssets.push(
+    Asset.fromModule(require("../assets/resources/img/it/16_it.jpg"))
+  );
+  imageAssets.push(
+    Asset.fromModule(require("../assets/resources/img/it/17_it.jpg"))
+  );
+  imageAssets.push(
+    Asset.fromModule(require("../assets/resources/img/it/18_it.jpg"))
+  );
+  imageAssets.push(
+    Asset.fromModule(require("../assets/resources/img/it/19_it.jpg"))
+  );
+  imageAssets.push(
+    Asset.fromModule(require("../assets/resources/img/it/20_it.jpg"))
+  );
+  imageAssets.push(
+    Asset.fromModule(require("../assets/resources/img/it/21_it.jpg"))
+  );
+  imageAssets.push(
+    Asset.fromModule(require("../assets/resources/img/it/22_it.jpg"))
+  );
+  imageAssets.push(
+    Asset.fromModule(require("../assets/resources/img/it/23_it.jpg"))
+  );
+  imageAssets.push(
+    Asset.fromModule(require("../assets/resources/img/it/24_it.jpg"))
+  );
+  imageAssets.push(
+    Asset.fromModule(require("../assets/resources/img/it/25_it.jpg"))
+  );
+  imageAssets.push(
+    Asset.fromModule(require("../assets/resources/img/it/26_it.jpg"))
+  );
+  imageAssets.push(
+    Asset.fromModule(require("../assets/resources/img/it/27_it.jpg"))
+  );
+  imageAssets.push(
+    Asset.fromModule(require("../assets/resources/img/it/28_it.jpg"))
+  );
+  imageAssets.push(
+    Asset.fromModule(require("../assets/resources/img/it/29_it.jpg"))
+  );
+  return imageAssets;
+}
 
 export default function SubScreen({ navigation, route }) {
   const [tab] = useState(route.params);
   const { language } = useContext(LanguageContext);
+
+  //const comicsAssetsMap = useMemo(() => getAssetsMap(getComicsAssets()), []);
+  const comicsAssetsMap = getAssetsMap(getComicsAssets());
 
   let checkScreenType = (tab) => {
     let result = 0;
@@ -72,6 +262,25 @@ export default function SubScreen({ navigation, route }) {
           'resultTab.push(parseInt(document.getElementById("scoreId").innerHTML));' +
           "window.ReactNativeWebView.postMessage(JSON.stringify(resultTab));})";
         break;
+      case 92: // Comics (replace image src tags)
+        generatedJS += `
+        var imgTags = document.querySelectorAll("img");
+        let assetsMap = ${JSON.stringify(comicsAssetsMap)};
+        window.ReactNativeWebView.postMessage('image tags' + imgTags);
+        for(let imgTag of imgTags){
+          imgTag.style.display = 'none';
+          imgTag.style.width = '100%';
+          window.ReactNativeWebView.postMessage('image tag src ' + imgTag.getAttribute('src'));
+          let originalSrc = imgTag.getAttribute('src');
+          window.ReactNativeWebView.postMessage('orig src ' + originalSrc);
+          let filename = originalSrc.substring(originalSrc.lastIndexOf('/') + 1);
+          window.ReactNativeWebView.postMessage('filename ' + filename);
+          let newSrc = assetsMap[filename];
+          imgTag.setAttribute('src', newSrc);
+          imgTag.style.display = 'block';
+        }
+        `;
+        break;
       default:
         //no javascript injection for nonquizz pages
         generatedJS = "";
@@ -86,6 +295,15 @@ export default function SubScreen({ navigation, route }) {
       <View style={localStyles.leafView}>
         <WebView
           textZoom={270}
+          onError={(event) => {
+            const { nativeEvent } = event;
+            console.warn("WebView error: ", nativeEvent);
+          }}
+          renderError={(errorName) => (
+            <View>
+              <Text>Aye! {errorName}</Text>
+            </View>
+          )}
           source={{
             html:
               tab.pages_lang[
@@ -100,12 +318,17 @@ export default function SubScreen({ navigation, route }) {
                     utilities.findLanguageIndex(tab.pages_lang, language)
                   ].text,
           }}
+          allowUniversalAccessFromFileURLs={true}
+          allowFileAccessFromFileURLs={true}
+          allowFileAccess={true}
+          originWhiteList={["*"]}
           onMessage={(event) => {
             console.log(event.nativeEvent.data);
-            storage.saveQuizScore(tab.id, event.nativeEvent.data);
+            if (tab.id == 95 || tab.id == 100)
+              storage.saveQuizScore(tab.id, event.nativeEvent.data);
           }}
           injectedJavaScript={generateJavaScript(tab.id)}
-          javaScriptEnabled={tab.id == 95 || tab.id == 100}
+          javaScriptEnabled={tab.id == 95 || tab.id == 100 || tab.id == 92}
           style={{
             flex: 1,
             height: height,
